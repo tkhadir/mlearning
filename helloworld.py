0b0df94e8251c404c9a1aa60c20646c936cb1d37
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2020-04-29 05:37:06 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+from sklearn import tree
+features = [[140, 1], [130, 1], [160, 0], [170, 0]]
+labels = ["s1", "s1", "s2", "s2"]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print("je suis capable de prédire la solution à " + str(clf.score(features, labels) * 100) + "%")
+print(clf.predict([[150, 0]]))
