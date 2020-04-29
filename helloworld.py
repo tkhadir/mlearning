127d5e5c7a5650e7a354e6b305cd5ffec8783c1c
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2020-04-29 05:55:29 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,4 @@ labels = ["s1", "s1", "s2", "s2"]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 print("je suis capable de prédire la solution à " + str(clf.score(features, labels) * 100) + "%")
-print(clf.predict([[150, 0]]))
+print("la solution que je propose est : " + clf.predict([[150, 0]])[0])
